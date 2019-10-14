a7d6eb668f8606618afb5ac0f77989ac972347fa
unknown <MIW@A-F004341.salsa.univ-amu.fr>
2019-10-14 16:19:58 +0200

    creation de upload s'il n'existe pas

--- 4/upload.php
+++ 4/upload.php
@@ -1,14 +1,28 @@
 <?php
 //types acceptés : pdf, png, jpg, gif
+if(!is_writable('upload/pdf/') || !is_writable('upload/image/')){
+    if(!file_exists('upload/')){
+        mkdir('upload/');
+        mkdir('upload/pdf');
+        mkdir('upload/image');
+    }
+    elseif(!file_exists('upload/pdf')){
+        mkdir('upload/pdf');
+    }
+    elseif(!file_exists('upload/image')){
+        mkdir('upload/image');
+    }
+}
 if(isset($_FILES['photo'])){
     var_dump($_FILES['photo']);
     if(($_FILES['photo']['error'] == UPLOAD_ERR_OK) && in_array($_FILES['photo']['type'], ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf', 'image/gif'])){
         $dossier = $_FILES['photo']['type'] == 'application/pdf' ? 'upload/pdf/' : 'upload/image/';
         $matches = null;
         $extension = preg_match_all('((pdf)|(png)|(jpg)|(jpeg)|(gif))',$_FILES['photo']['name'], $matches);
-        $fichier = time().'-'.$_POST['nom'].'.'.$matches[0][count($matches[0])-1];//ou on peut mettre le nom de fichier que l'on veut pour être certain d'éviter les doublons
+        $fichier = time().'-'.str_replace([' ',':'],'_',$_POST['nom']).'.'.$matches[0][count($matches[0])-1];//ou on peut mettre le nom de fichier que l'on veut pour être certain d'éviter les doublons
         if(move_uploaded_file($_FILES['photo']['tmp_name'], $dossier.$fichier)){
             //la fonction renvoie true, le fichier a bien été enregistré
+            echo 'Fichier '.$_POST['nom'].'.'.$matches[0][count($matches[0])-1].' enregistré avec succès';
         }else{
             echo 'echec de l\'upload.';
         }
@@ -33,4 +47,23 @@ if(isset($_FILES['photo'])){
     }elseif (!in_array($_FILES['photo']['type'], ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf', 'image/gif'])){
         echo 'Erreur : mauvais format de fichier';
     }
+}
+function find_all_files($dir)
+{
+    $result = [];
+    $root = scandir($dir);
+    foreach($root as $value)
+    {
+        if($value === '.' || $value === '..') {continue;}
+        if(is_file("$dir/$value")) {$result[]="$value";continue;}
+        foreach(find_all_files("$dir/$value") as $value)
+        {
+            $result[]=$value;
+        }
+    }
+    return $result;
+}
+$files = find_all_files('upload');
+foreach ($files as $val){
+    echo $val.'<br>';
 }
